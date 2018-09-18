333b1900da6999298c6fec2e94b7d0a392cfe295
Diogo Ide <diogoide@gmail.com>
2018-09-18 00:10:08 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,6 +30,7 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series
+  INNER JOIN characters ON series.author_id = chracters.author_id 
   WHERE species = 'human'
   ORDER BY length(title) DESC
   LIMIT 1;"
